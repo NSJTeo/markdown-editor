8bd4e256720a9972394f890b1450616f58106272
nsjteo <n.teomacdonald@gmail.com>
2022-04-29 11:03:20 -0700

    MardownEditor and Preview components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Header from './components/Header';
+import MarkdownEditor from './components/MarkdownEditor';
+import MarkdownPreview from './components/MarkdownPreview';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <MarkdownEditor />
+        <MarkdownPreview />
+      </main>
+    </>
+  );
 }
 
 export default App;
