610276ae3fe80cee96d325363f24ab4c8ded10b4
nsjteo <n.teomacdonald@gmail.com>
2022-04-29 10:58:19 -0700

    Create global styles component

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux';
 import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
+  </>
 );
