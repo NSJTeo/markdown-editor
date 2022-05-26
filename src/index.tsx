8c58b84ba2c6ce2a6d7be783a861ab35d8c1c1b0
nsjteo <n.teomacdonald@gmail.com>
2022-05-26 14:29:55 -0700

    Move global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { store } from './redux';
 import { Provider } from 'react-redux';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyles = createGlobalStyle`
-  *, *::before, *::after {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
-`;
+import { GlobalStyles } from './styles';
+import { store } from './redux';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,3 +3,4 @@ export { TitleContainer } from './TitleContainer';
 export { Title } from './Title';
 export { Icon } from './Icon';
 export { IconButton } from './IconButton';
+export { GlobalStyles } from './GlobalStyles';
